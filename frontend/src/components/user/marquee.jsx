715eb97242a499ce37d6608f87ebd15d1e91b98e
vijay <vijaykarri220910125@.gmail.com>
2025-06-25 13:14:35 +0530

    complaint care

---
+++ frontend/src/components/user/marquee.jsx
@@ -0,0 +1,13 @@
+ 
+
+function MarqueeText() {
+  return (
+    <div className="marquee-container">
+      <p className="marquee">
+        🚀 Welcome to my Complaint Analysis App! | 🎉 Vijay ! | Yuva !|Charan !|Shanmuja!
+      </p>
+    </div>
+  );
+}
+
+export default MarqueeText;
